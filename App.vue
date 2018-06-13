5b32a14f218db7e7556d8f61d5710a3cf8f296cb
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2018-06-13 11:56:04 +0900

    implement click event

--- App.vue
+++ App.vue
@@ -1,10 +1,26 @@
 <template>
   <view class="container">
     <text class="text-color-primary">
-      My Vue Native App
+      My Vue Native App!!!!!
     </text>
+    <text>{{count}}</text>
+    <button v-bind:title="message" v-bind:on-press="handleBtnPress" />
   </view>
 </template>
+
+<script>
+export default {
+  data: {
+    message: "Button",
+    count: 0
+  },
+  methods: {
+    handleBtnPress: function() {
+      this.count++;
+    }
+  }
+}
+</script>
 
 <style>
 .container {
